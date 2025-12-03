3b6e4262178335f2bd3ba1bd6aef12109d1f2032
jonas089 <jonaspauli089@gmail.com>
2025-12-03 10:54:21 +0100

    wip

--- crates/ev-state-queries/tests/integration.rs
+++ crates/ev-state-queries/tests/integration.rs
@@ -24,8 +24,9 @@ async fn test_run_indexer() {
         .join("data");
 
     let indexer = HyperlaneIndexer::default();
-    let store = Arc::new(HyperlaneMessageStore::from_path(storage_path).await.unwrap());
+    let mut store = HyperlaneMessageStore::from_path(storage_path).await.unwrap();
     store.reset_db().await.unwrap();
+    let store = Arc::new(store);
 
     let provider = ProviderBuilder::new()
         .connect_ws(WsConnect::new("ws://127.0.0.1:8546"))
