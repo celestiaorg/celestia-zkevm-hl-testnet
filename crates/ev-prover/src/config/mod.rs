54401377f4abdf68ba55ba1918b0469e87e9f4ea
jonas089 <jonaspauli089@gmail.com>
2026-01-16 12:16:15 +0100

    quickfix: merkle tree address update

--- crates/ev-prover/src/config/mod.rs
+++ crates/ev-prover/src/config/mod.rs
@@ -229,7 +229,7 @@ impl Default for EvmHyperlaneConfig {
     fn default() -> Self {
         Self {
             mailbox_address: "0xb1c938f5ba4b3593377f399e12175e8db0c787ff".into(),
-            merkle_tree_address: "0x1D957dA7A6988f5a9d2D2454637B4B7fea0Aeea5".into(),
+            merkle_tree_address: "0xFCb1d485ef46344029D9E8A7925925e146B3430E".into(),
         }
     }
 }
